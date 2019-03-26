131a9ec64b472a7a24b6696495e3a389897b158f
yash <yhdesai99@ymail.com>
2019-03-26 17:05:04 +0530

    moved add todo to a different acitivty

--- app/src/main/java/tech/shipr/tasksdev/MainActivity.java
+++ app/src/main/java/tech/shipr/tasksdev/MainActivity.java
@@ -4,15 +4,10 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
-import android.text.Editable;
-import android.text.InputFilter;
-import android.text.TextWatcher;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.ProgressBar;
 import android.widget.Toast;
@@ -42,8 +37,6 @@ public class MainActivity extends AppCompatActivity {
     private ListView mMessageListView;
     private MessageAdapter mMessageAdapter;
     private ProgressBar mProgressBar;
-    private EditText mMessageEditText;
-    private Button mSendButton;
 
     private String mUsername;
 
@@ -72,8 +65,6 @@ public class MainActivity extends AppCompatActivity {
         // Initialize references to views
         mProgressBar = findViewById(R.id.progressBar);
         mMessageListView = findViewById(R.id.messageListView);
-        mMessageEditText = findViewById(R.id.messageEditText);
-        mSendButton = findViewById(R.id.sendButton);
 
         // Initialize message ListView and its adapter
 
@@ -85,39 +76,6 @@ public class MainActivity extends AppCompatActivity {
         // Initialize progress bar
         mProgressBar.setVisibility(ProgressBar.INVISIBLE);
 
-        // Enable Send button when there's text to send
-        mMessageEditText.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-            }
-
-            @Override
-            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-                if (charSequence.toString().trim().length() > 0) {
-                    mSendButton.setEnabled(true);
-                } else {
-                    mSendButton.setEnabled(false);
-                }
-            }
-
-            @Override
-            public void afterTextChanged(Editable editable) {
-            }
-        });
-        mMessageEditText.setFilters(new InputFilter[]{new InputFilter.LengthFilter(DEFAULT_MSG_LENGTH_LIMIT)});
-
-        // Send button sends a message and clears the EditText
-        mSendButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                DeveloperToDo friendlyMessage = new DeveloperToDo(mMessageEditText.getText().toString());
-                mMessagesDatabaseReference.push().setValue(friendlyMessage);
-
-                // Clear input box
-                mMessageEditText.setText("");
-            }
-        });
 
 
         mAuthStateListener = new FirebaseAuth.AuthStateListener() {
@@ -258,4 +216,7 @@ public void onActivityResult(int requestCode, int resultcode, Intent data){
     }
 
 
+    public void openAddToDo(View view) {
+        startActivity(new Intent(MainActivity.this, AddToDoAcitivty.class));
+    }
 }
